7c82a22c2be2d04365ccce682fca4a659d9287fc
sathwararakesh <42351386+sathwararakesh@users.noreply.github.com>
2018-08-14 18:00:17 -1200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Free Bootstrap 4 Login/Register Forms By Wired Dots</title>
+        <meta name="description" content="Love Authority." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
+        <!--hero section-->
+        <section class="hero">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-8 mx-auto">
+                        <div class="card border-none">
+                            <div class="card-body">
+                                <div class="mt-2">
+                                    <img src="img/male.jpg" alt="Male" class="brand-logo mx-auto d-block img-fluid rounded-circle"/>
+                                </div>
+                                <p class="mt-4 text-white lead text-center">
+                                    Login Form
+                                </p>
+                                <div class="mt-4">
+                                    <form>
+                                        <div class="form-group">
+                                            <input type="email" class="form-control" id="email" value="" placeholder="Enter email address">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control" id="password" value="" placeholder="Enter password">
+                                        </div>
+                                        
+                                        <button type="submit" class="btn btn-primary float-right">Sign in</button>
+                                    </form>
+                                    <div class="clearfix"></div>
+                                    <p class="content-divider center mt-4"><span>or</span></p>
+                                </div>
+                                
+                                <p class="text-center">
+                                    Don't have an account yet? <a href="register.php">Sign Up Now</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                    
+                </div>
+            </div>
+        </section>
+
+    </body>
+</html>
